8ea46550b96e0d3b14e1d6e75a42b6ff06a48ab2
tanguopeng <guopengtan@rastar.com>
2018-04-16 14:40:57 +0800

    提交文件

---
+++ src/components/page/pos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="pos">
+    <left-nav>
+
+    </left-nav>
+    <el-row >
+            <el-col :span='7'>
+            我是订单栏
+            </el-col>
+            <!--商品展示-->
+            <el-col :span="17">
+             我是产品栏
+            </el-col>
+    </el-row>
+ </div>
+</template>
+
+<script>
+import LeftNav from './../common/leftNav.vue'
+export default {
+  name: 'pos',
+  data () {
+    return {
+      msg: 'a'
+    }
+  },
+  components: {
+    LeftNav
+  }
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
